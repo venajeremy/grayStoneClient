a7ee0a56549902aca8d34572ef6644bac9160834
venajeremy <venajeremy@gmail.com>
2025-05-29 13:48:47 -0700

    UPNP

--- Multiplayer.gd
+++ Multiplayer.gd
@@ -1,101 +1,21 @@
 extends Node
 
-signal noray_connected
+const port = 443
+const localport = 9999
 
-const NORAY_ADDRESS = "104.237.145.37"
-const NORAY_PORT = 8890
-const doNATPunchthrough = false
-
-var is_host = false
-var external_oid = ""
 @export var player_scene: PackedScene
 
-var peer = ENetMultiplayerPeer.new()
-
-func _ready():
-	Noray.on_connect_to_host.connect(on_noray_connected)
-	Noray.on_connect_nat.connect(handle_nat_connection)
-	Noray.on_connect_relay.connect(handle_relay_connection)
-	
-	Noray.connect_to_host(NORAY_ADDRESS, NORAY_PORT)
-
-func on_noray_connected():
-	print("Connected to Noray server")
-	
-	Noray.register_host()
-	await Noray.on_pid
-	await Noray.register_remote()
-	print("My oid: "+Noray.oid)
-	noray_connected.emit()
-
-func host():
-	print("Hosting")
-	
-	peer = ENetMultiplayerPeer.new()
-	peer.create_server(Noray.local_port)
-	multiplayer.multiplayer_peer = peer
-	multiplayer.peer_connected.connect(_add_player)
-	is_host = true
-	_add_player() # Add Host to lobby
+@onready var peer = ENetMultiplayerPeer.new()
 
 func _on_hud_create_server():
-	host()
-
-func join(oid):
-	#If NAT fails or if NAT is disabled relay will be used
-	if doNATPunchthrough:
-		Noray.connect_nat(oid)
-	else:
-		Noray.connect_relay(oid)
-	external_oid = oid
-
-func _on_hud_join_server(oid):
-	join(oid)
-
-func handle_nat_connection(address, port):
-	var err = await connect_to_server(address, port)
+	var error = peer.create_server(port)
+	if error:
+		return error
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(_add_player)
+	_add_player() # Add Host to lobby
 	
-	if err != OK && !is_host:
-		print("NAT failed, using relay")
-		Noray.connect_relay(external_oid)
-		err = OK
-	
-	return err
-
-func handle_relay_connection(address, port):
-	return await connect_to_server(address, port)
-
-func connect_to_server(address, port):
-	var err = OK
-	
-	if !is_host:
-		var udp = PacketPeerUDP.new()
-		udp.bind(Noray.local_port)
-		udp.set_dest_address(address, port)
-		
-		err = await PacketHandshake.over_packet_peer(udp)
-		udp.close()
-		
-		if err != OK:
-			if err != ERR_BUSY:
-				print("Handshake failed")
-				return err
-		else:
-			print("Handshake success")
-		
-		peer = ENetMultiplayerPeer.new()
-		err = peer.create_client(address, port, 0, 0, 0, Noray.local_port)
-		
-		if err != OK:
-			return err
-		
-		multiplayer.multiplayer_peer = peer
-		
-		return OK
-	else:
-		err = await PacketHandshake.over_enet(multiplayer.multiplayer_peer.host, address, port)
-	
-	return err
+	upnp_setup()
 
 func _add_player(id = 1):
 	var player = player_scene.instantiate()
@@ -105,6 +25,10 @@ func _add_player(id = 1):
 func exit_game(id):
 	multiplayer.peer_disconnected.connect(del_player)
 	del_player(id)
+
+func _on_hud_join_server(ip = "localhost"):
+	peer.create_client(ip, port, 0, 0, 0, localport)
+	multiplayer.multiplayer_peer = peer
 	
 func del_player(id):
 	rpc("_del_player",id)
@@ -112,4 +36,20 @@ func del_player(id):
 @rpc("any_peer","call_local")
 func _del_player(id):
 	get_node(str(id)).queue_free()
-	
+
+func upnp_setup():
+	# UPNP queries take some time.
+	var upnp = UPNP.new()
+	var err = upnp.discover()
+
+	if err != OK:
+		push_error(str(err))
+		print("Error In UPNP Setup: "+err)
+		return
+
+	if upnp.get_gateway() and upnp.get_gateway().is_valid_gateway():
+		upnp.add_port_mapping(port, port, ProjectSettings.get_setting("application/config/name"), "UDP")
+		upnp.add_port_mapping(port, port, ProjectSettings.get_setting("application/config/name"), "TCP")
+		print("UPNP Setup Successful")
+	else:
+		print("Could Not Get UPNP Gateway")
